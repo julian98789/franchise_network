ace9e194cea0a631ac3c5863d7ad62b09e6b7376
Julian Esteban <julian.gomeza@pragma.com.co>
2025-06-21 17:38:30 -0500

    Create endpoint to add new breanch

---
+++ src/main/java/com/franchise_network/franchise/domain/api/IBranchServicePort.java
@@ -0,0 +1,8 @@
+package com.franchise_network.franchise.domain.api;
+
+import com.franchise_network.franchise.domain.model.Branch;
+import reactor.core.publisher.Mono;
+
+public interface IBranchServicePort {
+    Mono<Branch> addBranchToFranchise(Branch branch);
+}
---
+++ src/main/java/com/franchise_network/franchise/infrastructure/entrypoints/dto/BranchDTO.java
@@ -0,0 +1,13 @@
+package com.franchise_network.franchise.infrastructure.entrypoints.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class BranchDTO {
+    private String name;
+    private Long franchiseId;
+}
